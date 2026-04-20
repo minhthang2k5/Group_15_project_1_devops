ebd0ca6beaf4ddbf93bfb6f94b11f7ca964ac1d6
minh thang <trinhhominhthang2k5@gmail.com>
2026-04-20 15:01:21 +0700

    Check web hook in cart service

--- cart/src/main/java/com/yas/cart/CartApplication.java
+++ cart/src/main/java/com/yas/cart/CartApplication.java
@@ -10,6 +10,7 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 @EnableConfigurationProperties({ServiceUrlConfig.class, CorsConfig.class})
 public class CartApplication {
 
+    // Entry point for the cart service application.
     public static void main(String[] args) {
         SpringApplication.run(CartApplication.class, args);
     }
