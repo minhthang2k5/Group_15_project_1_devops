a444a8bdec93aa87ee84a726c5b413b555214048
huuthang-0355 <vohuuthang3110@gmail.com>
2026-04-28 22:29:25 +0700

    add more unit test for product service

---
+++ product/src/test/java/com/yas/product/utils/ProductConverterTest.java
@@ -0,0 +1,22 @@
+package test.java.com.yas.product.utils;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+
+class ProductConverterTest {
+
+    @Test
+    void test_to_slug_trims_lowercases_and_replaces() {
+        String result = ProductConverter.toSlug("  Hello World!  ");
+
+        assertThat(result).isEqualTo("hello-world-");
+    }
+
+    @Test
+    void test_to_slug_collapses_dashes_and_removes_leading() {
+        String result = ProductConverter.toSlug("--Hello  World");
+
+        assertThat(result).isEqualTo("hello-world");
+    }
+}
