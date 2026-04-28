2a3ebf5b92cee9986056b84b3b965bb55d9b86f6
huuthang-0355 <vohuuthang3110@gmail.com>
2026-04-28 21:52:11 +0700

    trigger for build in jenkins

--- cart/src/main/java/com/yas/cart/CartApplication.java
+++ cart/src/main/java/com/yas/cart/CartApplication.java
@@ -11,7 +11,7 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 public class CartApplication {
 
     public static void main(String[] args) {
-        // test for cart
+        // test for ca
         SpringApplication.run(CartApplication.class, args);
     }
 }
