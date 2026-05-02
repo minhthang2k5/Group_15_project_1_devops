f9ed000f73acaa00b817d7c9b37d9850acbd78e9
minh thang <trinhhominhthang2k5@gmail.com>
2026-05-02 19:53:02 +0700

    chore: fix bug diff jenkins

--- cart/src/it/java/com/yas/cart/controller/CartItemControllerIT.java
+++ cart/src/it/java/com/yas/cart/controller/CartItemControllerIT.java
@@ -1,5 +1,5 @@
 package com.yas.cart.controller;
-//add scans
+//add scansa
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.is;
 import static org.mockito.ArgumentMatchers.anyLong;
