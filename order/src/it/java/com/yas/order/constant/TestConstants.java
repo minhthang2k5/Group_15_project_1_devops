03fa96f766ebe23673fe94394355fd9b04361058
minh thang <trinhhominhthang2k5@gmail.com>
2026-05-03 12:26:52 +0700

    chore: scan security

--- order/src/it/java/com/yas/order/constant/TestConstants.java
+++ order/src/it/java/com/yas/order/constant/TestConstants.java
@@ -1,5 +1,5 @@
 package com.yas.order.constant;
-// test scans
+// test scans for orderservice
 public class TestConstants {
     public static final String CIRCUIT_BREAKER_NAME = "restCircuitBreaker";
 }
